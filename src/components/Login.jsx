1f9e23239aaaba8d478ebfab45dd8c7c52b6770a
Sachin <sachin333777@outlook.com>
2024-03-29 14:35:55 +0530

    setting enviroment variables

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,38 @@
-
+import { useState } from "react";
+import authSpotify from "../utils/Spotify";
 export const Login = () => {
+  const [accessToken, setAccessToken] = useState("")
+
+
+
   return (
     <>
-    <div>
-      <button>login with spotify</button>
-    </div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_1280.jpg"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <button
+              onClick={}
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-green-700 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              >
+                Sign in with Spotify
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
---
+++ src/conf/conf.js
@@ -0,0 +1,5 @@
+const conf = {
+  client_id: String(import.meta.env.VITE_CLIENT_ID)
+}
+
+export default conf;
